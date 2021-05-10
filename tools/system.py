6caaa0a989e010cdcb3f523f6691a47fc5310693
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-05-10 09:42:14 -0700

    reformat the system

--- tools/system.py
+++ tools/system.py
@@ -8,7 +8,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import platform
 
-
 import tensorflow as tf
 from tensorflow.python.client import device_lib
 
@@ -20,35 +19,25 @@ def system_status(args):
     re["Processor"] = platform.processor()
     for i in devices_:
         if i.device_type == "CPU":
-<<<<<<< HEAD
             re["Processor_MEMORY_LIMIT"] = i.memory_limit
-=======
-            re["Processor_MEMORY_LIMIT"]=i.memory_limit
->>>>>>> e84ed99257ff9b213c0eb9b7b90df6ce0f3f719c
+
+            re["Processor_MEMORY_LIMIT"] = i.memory_limit
 
     idx = 0
     for dev in devices_:
         if dev.device_type == "GPU":
-<<<<<<< HEAD
             prefix = "GPU_" + str(idx)
             re[prefix + "_NAME"] = dev.physical_device_desc
             re[prefix + "_MEMORY_LIMIT"] = dev.memory_limit
             idx += 1
-=======
-            prefix = "GPU_"+str(idx)
-            re[prefix+"_NAME"] = dev.physical_device_desc
-            re[prefix + "_MEMORY_LIMIT"]=dev.memory_limit
-            idx+=1
->>>>>>> e84ed99257ff9b213c0eb9b7b90df6ce0f3f719c
+
+            prefix = "GPU_" + str(idx)
+            re[prefix + "_NAME"] = dev.physical_device_desc
+            re[prefix + "_MEMORY_LIMIT"] = dev.memory_limit
+            idx += 1
 
     re["CUDA Enabled"] = tf.test.is_built_with_cuda()
     for key, value in re.items():
         print(f"$ {key}: {value}")
 
-<<<<<<< HEAD
     return re
-=======
-
-
-system_status('ar')
->>>>>>> e84ed99257ff9b213c0eb9b7b90df6ce0f3f719c
