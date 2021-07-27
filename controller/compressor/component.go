461d53287f2d911c11d6ba97edcf878ce4ced7c8
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-07-27 01:56:56 -0700

    complemte Zipper

--- controller/compressor/component.go
+++ controller/compressor/component.go
@@ -2,6 +2,10 @@ package compressor
 
 import (
 	log "../logging"
+	"archive/zip"
+	"fmt"
+	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
 )
@@ -12,35 +16,73 @@ var (
 )
 
 
-func Zip(filename string,outputPath string, targetPath string){
+func Zip(filename string,outputPath string, targetPath string)error{
+
+	// check the target file folder existence
 	_,err:=os.Stat(targetPath)
 	if os.IsNotExist(err){
 		log.Println(log.DANG,"$ Target folder path is not exists")
 	}
 
+	// check output folder existence, if not, create it
 	_,err=os.Stat(outputPath)
 	if os.IsNotExist(err){
 		err=os.Mkdir(outputPath,0755)
-
 		if err!=nil {
-			log.Println(log.DANG, "$ Can`nt create folder path")
-		}else{
-			log.Println(log.SUCCESS,"$ Create folder path")
+			return err
 		}
 	}
 
+	// create full path of output
 	fullFilePath := filepath.Join(outputPath,filename)
 
-	log.Println(log.SUCCESS,fullFilePath)
+	archive,err:= os.Create(fullFilePath)
 
-	//r,err := zip.OpenReader(targetPath)
-	//if err!=nil{
-	//	log.Println(log.DANG,"$ Problem in reading source")
-	//}
-	//
-	//defer r.Close()
+	if err!=nil{
+		return err
+	}
 
+	defer archive.Close()
 
 
+	// perform zip
+	zipwriter:=zip.NewWriter(archive)
+
+	defer zipwriter.Close()
+
+	f1,err:=os.Open(targetPath)
+	if err!=nil{
+		return err
+	}
+
+	defer f1.Close()
+
+	fmt.Println(f1)
+
+	var files[]string
+
+	err = filepath.Walk(targetPath, func(path string, info fs.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+
+
+	for _,file := range files{
+		if filepath.Ext(file)!=""{
+			f1, err := os.Open(file)
+			if err != nil {
+				return err
+			}
+
+			w, err := zipwriter.Create(file)
+
+			if _,err:=io.Copy(w,f1);err!=nil{
+				return err
+			}
+
+		}
+	}
+
+	return nil
 
 }
--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"./controller/compressor"
+	"fmt"
 )
 
 func main(){
 
 	var filename = "armin.zip"
-	var target = "/tmp/armin"
-	var outputPath = "./data/temp"
-    compressor.Zip(filename,outputPath,target)
+	var target = "data/temp"
+	var outputPath = "data"
+    err:=compressor.Zip(filename,outputPath,target)
+
+    fmt.Println(err)
+
 }
