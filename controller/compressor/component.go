5e04147efff81ecba31150cd422bec02b6ecb452
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-07-27 02:40:01 -0700

    add unzip function

--- controller/compressor/component.go
+++ controller/compressor/component.go
@@ -3,10 +3,12 @@ package compressor
 import (
 	log "../logging"
 	"archive/zip"
+	"fmt"
 	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var (
@@ -14,8 +16,8 @@ var (
 	err         error
 )
 
-func Zip(filename string, outputPath string, targetPath string) error {
-
+func Zip(filename string, outputPath string, targetPath string) (string,error) {
+	// function for zipping a given root
 	// check the target file folder existence
 	_, err := os.Stat(targetPath)
 	if os.IsNotExist(err) {
@@ -27,7 +29,7 @@ func Zip(filename string, outputPath string, targetPath string) error {
 	if os.IsNotExist(err) {
 		err = os.Mkdir(outputPath, 0755)
 		if err != nil {
-			return err
+			return "",err
 		}
 	}
 
@@ -37,7 +39,7 @@ func Zip(filename string, outputPath string, targetPath string) error {
 	archive, err := os.Create(fullFilePath)
 
 	if err != nil {
-		return err
+		return "",err
 	}
 
 	defer archive.Close()
@@ -49,7 +51,7 @@ func Zip(filename string, outputPath string, targetPath string) error {
 
 	f1, err := os.Open(targetPath)
 	if err != nil {
-		return err
+		return "",err
 	}
 
 	defer f1.Close()
@@ -65,15 +67,81 @@ func Zip(filename string, outputPath string, targetPath string) error {
 		if filepath.Ext(file) != "" {
 			f1, err := os.Open(file)
 			if err != nil {
-				return err
+				return "",err
 			}
 			w, err := zipwriter.Create(file)
 			if _, err := io.Copy(w, f1); err != nil {
-				return err
+				return "",err
 			}
 
 		}
 	}
 
-	return nil
+	return "",nil
+}
+
+
+func Unzip(target string,outputPath string)([]string,error){
+	// function for unzipping a file
+	// check directory existence
+
+	var filenames []string
+	_,err:= os.Stat(target)
+	if os.IsNotExist(err){
+		return filenames,err
+	}
+
+	_,err = os.Stat(outputPath)
+	if os.IsNotExist(err){
+		return filenames,err
+	}
+
+	reader,err:=zip.OpenReader(target)
+	if err!=nil{
+		return filenames,err
+	}
+
+	defer reader.Close()
+
+	for _,file:= range reader.File{
+		fPath:= filepath.Join(outputPath,file.Name)
+
+		if !strings.HasPrefix(fPath,filepath.Clean(outputPath)+string(os.PathSeparator)){
+			return filenames,fmt.Errorf("%s: illegal file path",fPath)
+		}
+
+		filenames = append(filenames,fPath)
+
+		if file.FileInfo().IsDir(){
+			os.MkdirAll(fPath,os.ModePerm)
+			continue
+		}
+
+		// Make file
+		if err = os.MkdirAll(filepath.Dir(fPath),os.ModePerm);err!=nil{
+			return filenames,err
+		}
+
+		outFile, err := os.OpenFile(fPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return filenames, err
+		}
+
+		rc, err := file.Open()
+		if err != nil {
+			return filenames, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return filenames, err
+		}
+
+	}
+
+	return filenames, nil
 }
--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 
 func main(){
 
-	var filename = "armin.zip"
-	var target = "data/temp"
-	var outputPath = "data"
-    err:=compressor.Zip(filename,outputPath,target)
+	//var filename = "armin.zip"
+	var target = "data/armin.zip"
+	var outputPath = "data/temp"
+    filenames,err:=compressor.Unzip(target,outputPath)
 
     fmt.Println(err)
+    fmt.Println(filenames)
 
 }
