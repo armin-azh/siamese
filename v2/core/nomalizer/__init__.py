868ae0bac4589062aa035c03cb9a9b6335914b09
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-08-08 04:06:15 -0700

    add normalizer to __init__

---
+++ v2/core/nomalizer/__init__.py
@@ -0,0 +1,7 @@
+from ._base import BaseNormalizer
+from ._conv import GrayScaleConvertor
+from ._norm import FaceNetNormalizer
+
+FaceNetNormalizer = FaceNetNormalizer
+GrayScaleConvertor = GrayScaleConvertor
+BaseNormalizer = BaseNormalizer
