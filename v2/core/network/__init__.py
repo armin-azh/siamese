bc0a6d82ef19f780f170d155b52cd916b59366e6
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-08-11 04:30:45 -0700

    add source modul

--- v2/core/network/__init__.py
+++ v2/core/network/__init__.py
@@ -1,7 +1,11 @@
 from .base import BaseModel
 from ._face_detector import FaceDetector
 from ._recognizer import FaceNetModel
+from ._hpe import HeadPoseEstimatorModel
+from ._mask import MaskClassifierModel
 
 BaseModel = BaseModel
 MultiCascadeFaceDetector = FaceDetector
 FaceNetModel = FaceNetModel
+HPEModel = HeadPoseEstimatorModel
+MaskModel = MaskClassifierModel
