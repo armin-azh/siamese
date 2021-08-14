7f98015530f634038dd8c8aa8d93d986576d99e2
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-08-14 05:19:53 -0700

    add logger into BaseSource

--- v2/core/source/_base.py
+++ v2/core/source/_base.py
@@ -4,6 +4,9 @@ from typing import Tuple
 from collections import deque
 from threading import Thread
 from pathlib import Path
+from datetime import datetime
+
+from v2.tools.logger import FileLogger, ConsoleLogger
 
 
 class BaseSource:
@@ -17,6 +20,11 @@ class BaseSource:
         self._frame_dequeue = deque(maxlen=self._queue_size)
         self._thread = Thread(target=self._get_frame, args=())
         self._display_log = display
+        _cu = datetime.strftime(datetime.now(), '%Y%m%d-%H%M%S')
+        self._log_path = logg_path.joinpath(f"camera/{self._id}/{_cu}.log")
+        self._log_path.mkdir(parents=True, exist_ok=True)
+        self._file_logger = FileLogger(self._log_path, self._id)
+        self._console_logger = ConsoleLogger()
 
     def _get_frame(self):
         pass
