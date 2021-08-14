a83445bb9d0250537ed8725bd88220562637de4a
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-08-14 11:05:10 -0700

    add defaul source model to __init__

---
+++ v2/core/source/__init__.py
@@ -0,0 +1,7 @@
+from ._base import BaseSource
+from ._defaults import FileSource, ProtocolSource, WebCamSource
+
+BaseSourceModel = BaseSource
+FileSourceModel = FileSource
+ProtocolSourceModel = ProtocolSource
+WebCamSourceModel = WebCamSource
