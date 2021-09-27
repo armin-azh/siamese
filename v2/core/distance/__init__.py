1129f4cc79627bd3606703cd67a70b3703b8b5fe
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-09-27 01:50:20 -0700

    add Mahala to the top of the module

--- v2/core/distance/__init__.py
+++ v2/core/distance/__init__.py
@@ -1,8 +1,13 @@
 from .base import BaseDistance
-from ._dist import CosineDistance, SklearnCosineDistance, EuclideanDistance,SklearnEuclideanDistance
+from ._dist import (CosineDistance,
+                    SklearnCosineDistance,
+                    EuclideanDistance,
+                    SklearnEuclideanDistance,
+                    SiPyMahalanobisDistance512)
 
 Distance = BaseDistance
 CosineDistanceV1 = CosineDistance
 CosineDistanceV2 = SklearnCosineDistance
 EuclideanDistanceV1 = EuclideanDistance
 EuclideanDistanceV2 = SklearnEuclideanDistance
+MahalanobisDistanceV1 = SiPyMahalanobisDistance512
